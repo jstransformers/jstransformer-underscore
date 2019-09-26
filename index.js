e18bdb91c24912afc34eb811defe111e381c8bf0
Rob Loach <robloach@gmail.com>
2019-09-26 12:02:51 -0400

    Fix eslint

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const _ = require('underscore')
 
 exports.name = 'underscore'
 exports.outputFormat = 'html'
+
 exports.compile = function (source, options) {
   return _.template(source, options)
 }
+
 exports.compileClient = function (source, options) {
   return _.template(source, options).source
 }
